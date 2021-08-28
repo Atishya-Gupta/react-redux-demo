2d93a500634a3f637bbb248bc0acd5e392854f0e
Atishya Gupta <atish3604@gmail.com>
2021-08-28 11:24:56 +0530

    Added Cake component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import './App.css';
-
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import CakeContainer from './components/CakeContainer'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Provider store={store}>
+      <div className='App'>
+        <CakeContainer />
+      </div>
+    </Provider>
+  )
 }
 
 export default App;
