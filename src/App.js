4a9d50b6ff495de41e5f3224670f8dc6b7f034ba
Atishya Gupta <atish3604@gmail.com>
2021-08-28 14:06:29 +0530

    Added CakeAndIceCream Container

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
 import { Provider } from 'react-redux'
 import store from './redux/store'
-import CakeContainer from './components/CakeContainer'
 import HooksCakeContainer from './components/HooksCakeContainer'
 import IceCreamContainer from './components/IceCreamContainer'
+import CakeAndIceCreamContainer from './components/CakeAndIceCreamContainer'
 function App() {
   return (
     <Provider store={store}>
       <div className='App'>
         <IceCreamContainer />
         <HooksCakeContainer />
-        <CakeContainer />
+        <CakeAndIceCreamContainer />
       </div>
     </Provider>
   )
